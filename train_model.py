af4e0e45f8364b36c23fd069db50100f0d965d7b
Jacob <simplafyer@gmail.com>
2024-11-22 13:48:35 -0500

    halfway update to train_model completion

--- train_model.py
+++ train_model.py
@@ -12,15 +12,14 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
-project_path = "Your path here"
+
+project_path = C:\Users\Jacob\Desktop\School\D501\Workspace\Deploying-a-Scalable-ML-Pipeline-with-FastAPI
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.load_csv('census.csv')
 
-# TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = None, None# Your code here
+
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,11 +35,11 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
-    )
+    train,
+    categorical_features=cat_features,
+    label='salary',  
+    training=True,
+)
 
 X_test, y_test, _, _ = process_data(
     test,
@@ -51,8 +50,8 @@ X_test, y_test, _, _ = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+
+model = train_model(X_train,y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
